a9484c9e1e3a05f320c9d049a66c8a8548dfd92f
DebRC <roychowdhurydebdoot@gmail.com>
2023-02-21 16:54:25 +0530

    Added Image and Remarks

--- App/meat_gradation_app/lib/models/meat.dart
+++ App/meat_gradation_app/lib/models/meat.dart
@@ -1,28 +1,38 @@
+import 'dart:io';
+
 class Meat {
+  File? meatImage;
   String? meatType;
   double? meatTypeConfidence;
   double? consumableConfidence;
   double? nonConsumableConfidence;
+  String? remarks;
 
   Meat(
-      {this.meatType,
+      {this.meatImage,
+      this.meatType,
       this.meatTypeConfidence,
       this.consumableConfidence,
-      this.nonConsumableConfidence});
+      this.nonConsumableConfidence,
+      this.remarks});
 
   Meat.fromJson(Map<String, dynamic> json) {
+    meatImage = json['meatImage'];
     meatType = json['meatType'];
     meatTypeConfidence = json['meatTypeConfidence'];
     consumableConfidence = json['consumableConfidence'];
     nonConsumableConfidence = json['nonConsumableConfidence'];
+    remarks = json['remarks'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['meatImage'] = meatImage;
     data['meatType'] = meatType;
     data['meatTypeConfidence'] = meatTypeConfidence;
     data['consumableConfidence'] = consumableConfidence;
     data['nonConsumableConfidence'] = nonConsumableConfidence;
+    data['remarks'] = remarks;
     return data;
   }
 }
