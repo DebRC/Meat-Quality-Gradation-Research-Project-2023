ed1f9f7d61e2f3a8744bb2730cba48d289040429
DebRC <roychowdhurydebdoot@gmail.com>
2023-02-21 17:41:24 +0530

    Added Button

--- App/meat_gradation_app/lib/screens/result_screen.dart
+++ App/meat_gradation_app/lib/screens/result_screen.dart
@@ -27,44 +27,184 @@ class _ResultScreenState extends State<ResultScreen> {
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Prediction Result"),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: const Icon(
+            Icons.arrow_back_ios_new_rounded,
+            size: 25,
+            color: Color.fromARGB(218, 131, 19, 17),
+          ),
+        ),
+        title: const Text(
+          "Prediction Result",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(218, 131, 19, 17),
+          ),
+        ),
+        backgroundColor: Color.fromRGBO(243, 232, 233, 1),
+        elevation: 0,
+        centerTitle: true,
       ),
       body: ListView(
         children: [
           SingleChildScrollView(
-            child: Column(
-              children: [
-                Image.file(widget.prediction["meatImage"]),
-                Text("Meat Type :: ${widget.prediction["meatType"].toString()}",
-                    style: TextStyle(
-                        fontFamily: 'Brand',
-                        fontSize: width * 0.055,
-                        color: const Color(0xFF818181))),
-                Text(
-                    ("Meat Type Confidence :: ${(widget.prediction["meatTypeConfidence"] * 100).toString().substring(0, 6)}%"),
-                    style: TextStyle(
-                        fontFamily: 'Brand',
-                        fontSize: width * 0.055,
-                        color: const Color(0xFF818181))),
-                Text(
-                    ("Consumable :: ${(widget.prediction["consumableConfidence"] * 100).toString().substring(0, 6)}%"),
-                    style: TextStyle(
-                        fontFamily: 'Brand',
-                        fontSize: width * 0.055,
-                        color: const Color(0xFF818181))),
-                Text(
-                    ("Non-Consumable :: ${(widget.prediction["nonConsumableConfidence"] * 100).toString().substring(0, 6)}%"),
-                    style: TextStyle(
-                        fontFamily: 'Brand',
-                        fontSize: width * 0.055,
-                        color: const Color(0xFF818181))),
-                Text("Remarks :: ${widget.prediction["remarks"].toString()}",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontFamily: 'Brand',
-                        fontSize: width * 0.055,
-                        color: const Color(0xFF818181))),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 15),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20), // Image border
+                      child: SizedBox.fromSize(
+                        size: Size.fromRadius(width / 3), // Image radius
+                        child: Image.file(widget.prediction["meatImage"],
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text("Meat Type :: ",
+                                style: TextStyle(
+                                    fontFamily: 'Brand',
+                                    fontSize: width * 0.055,
+                                    color: Color.fromARGB(255, 0, 0, 0))),
+                            Flexible(
+                              child: Text(
+                                  widget.prediction["meatType"].toString(),
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  softWrap: false,
+                                  style: TextStyle(
+                                      fontFamily: 'Brand',
+                                      fontSize: width * 0.055,
+                                      color:
+                                          Color.fromARGB(255, 138, 136, 136))),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Text("Meat Type Confidence :: ",
+                                style: TextStyle(
+                                    fontFamily: 'Brand',
+                                    fontSize: width * 0.055,
+                                    color: Color.fromARGB(255, 0, 0, 0))),
+                            Flexible(
+                              child: Text(
+                                  "${(widget.prediction["meatTypeConfidence"] * 100).toString().substring(0, 6)}%",
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  softWrap: false,
+                                  style: TextStyle(
+                                      fontFamily: 'Brand',
+                                      fontSize: width * 0.055,
+                                      color:
+                                          Color.fromARGB(255, 138, 136, 136))),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Text("Consumable :: ",
+                                style: TextStyle(
+                                    fontFamily: 'Brand',
+                                    fontSize: width * 0.055,
+                                    color: Color.fromARGB(255, 0, 0, 0))),
+                            Flexible(
+                              child: Text(
+                                  "${(widget.prediction["consumableConfidence"] * 100).toString().substring(0, 6)}%",
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  softWrap: false,
+                                  style: TextStyle(
+                                      fontFamily: 'Brand',
+                                      fontSize: width * 0.055,
+                                      color:
+                                          Color.fromARGB(255, 138, 136, 136))),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Text("Non-Consumable :: ",
+                                style: TextStyle(
+                                    fontFamily: 'Brand',
+                                    fontSize: width * 0.055,
+                                    color: Color.fromARGB(255, 0, 0, 0))),
+                            Flexible(
+                              child: Text(
+                                  "${(widget.prediction["nonConsumableConfidence"] * 100).toString().substring(0, 6)}%",
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  softWrap: false,
+                                  style: TextStyle(
+                                      fontFamily: 'Brand',
+                                      fontSize: width * 0.055,
+                                      color:
+                                          Color.fromARGB(255, 138, 136, 136))),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Text("Remarks :: ",
+                                style: TextStyle(
+                                    fontFamily: 'Brand',
+                                    fontSize: width * 0.055,
+                                    color: Color.fromARGB(255, 0, 0, 0))),
+                            Flexible(
+                              child: Text(
+                                  "${widget.prediction["remarks"].toString()}",
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  softWrap: false,
+                                  style: TextStyle(
+                                      fontFamily: 'Brand',
+                                      fontSize: width * 0.055,
+                                      color:
+                                          Color.fromARGB(255, 138, 136, 136))),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 50, vertical: 50),
+                    child: BigButton(
+                        title: "GRADE ANOTHER",
+                        fontColor: Colors.white,
+                        backgroundColor: Color.fromARGB(255, 224, 50, 20),
+                        // Function's getting called after pressing Get Started button
+                        onPressed: () async {
+                          Navigator.pop(context);
+                        }),
+                  )
+                ],
+              ),
             ),
           )
         ],
