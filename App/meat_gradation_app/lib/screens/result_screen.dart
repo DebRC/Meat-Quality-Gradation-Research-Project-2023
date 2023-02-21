a42f7348b40dd12d44c0e927a65a40c7a26091c2
DebRC <roychowdhurydebdoot@gmail.com>
2023-02-21 12:30:22 +0530

    Added File

---
+++ App/meat_gradation_app/lib/screens/result_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ResultScreen extends StatefulWidget {
+  const ResultScreen({super.key});
+
+  @override
+  State<ResultScreen> createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
