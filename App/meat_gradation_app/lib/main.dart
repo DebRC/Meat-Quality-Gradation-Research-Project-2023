68228818306bdb58a5a4b954f0c7cc97766548bf
DebRC <roychowdhurydebdoot@gmail.com>
2023-02-21 12:28:14 +0530

    Deleted unnecessary imports

--- App/meat_gradation_app/lib/main.dart
+++ App/meat_gradation_app/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:meat_gradation_app/screens/main_screen.dart';
-import 'package:tflite_flutter/tflite_flutter.dart';
 
 var route;
 
